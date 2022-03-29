18e9f8de7b817b04474016ca6cd20bfcf1765e5b
LAPTOP-LEDH1DET\MrBones <salasdaniel1998@gmail.com>
2022-03-29 18:09:18 +0200

    Alter this shouldn't have been so horrible jeez. Jump and held down Jump implemented, proper collision and ground check behaviors, facing sprite direction implemented but not used yet. Cinemachine imported

--- Ethereal/Assets/Scripts/PlayerController.cs
+++ Ethereal/Assets/Scripts/PlayerController.cs
@@ -5,23 +5,36 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Walking Speed")]
+    [SerializeField] private int _speed;
+
+    [Header("Jump Power")]
+    //[Range(3000,4000)] //this is for AddForceOnly big boy number
+    [SerializeField] private float _jumpPower;
+
+    [Header("Gravity while falling")]
+    [SerializeField] private float _fallingGravity;
+    
     [SerializeField] private Transform _groundCheckPos;
     [SerializeField] private GameObject _sprite;
-    [SerializeField] private int _speed;
     [SerializeField] private float _groundCheckRadius;
-    [SerializeField] private float _jumpPower;
+    [SerializeField] private float _jumpTime;
 
     private Rigidbody2D _rigidbody;
     private Vector3 _playerScale;
     private Vector2 _direction;
+    private float _originalGravity;
+    private float _jumpTimeCounter;
     private bool _isGrounded;
     private bool _isTryingToJump = false;
+    private bool _isJumping = false;
 
     //Can rename to ground we can jump from or something of the sort as we add more surfaces
     public LayerMask Ground;
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _originalGravity = _rigidbody.gravityScale;
         //_playerScale = _sprite.transform.localScale;
         
     }
@@ -30,6 +43,8 @@ public class PlayerController : MonoBehaviour
         UpdatePlayerInput();
         
         _isGrounded = Physics2D.OverlapCircle(_groundCheckPos.position, _groundCheckRadius,Ground);
+        
+        //checking for Jump Input
         if (Input.GetKeyDown(KeyCode.Space))
         {
             _isTryingToJump = true;
@@ -38,16 +53,54 @@ public class PlayerController : MonoBehaviour
         {
             _isTryingToJump = false;
         }
-        Debug.Log($"Is grounded: {_isGrounded}");
-        Debug.Log($"Is trying to Jump {_isTryingToJump}");
-        //if (_isGrounded && _isTryingToJump)
-        //if (_isGrounded && Input.GetKeyDown(KeyCode.Space)) 
-        //{
-        //    _rigidbody.AddForce(Vector2.up*_jumpPower,ForceMode2D.Impulse);
-        //    Debug.Log("Jumpy Jump");
-        //}
 
+
+        Debug.Log($"Is grounded: {_isGrounded}");
+        Debug.Log($"Is trying to Jump: {_isTryingToJump}");
+
+        // Setting according gravity scale
+        if (_isGrounded)
+        {
+            _rigidbody.gravityScale = _originalGravity;
+        }
+        else
+        {
+            _rigidbody.gravityScale = _fallingGravity;
+        }
+        
+
+        //Checking for Jump, extract method
+        //_isTryingToJump + _isJumping and
+        //then the same holding key or not logic for DOUBLE JUMP
+        if (_isGrounded && _isTryingToJump)
+        {
+            Jump();
+        }
+        if (Input.GetKey(KeyCode.Space) && _isJumping)
+        {
+            ExtendedJump();
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            _isJumping = false;
+        }
+        //{
+        //    _rigidbody.gravityScale *= _multiplier;
+        //}
         //FacingDirection();
+    }
+
+    private void ExtendedJump()
+    {
+        if (_jumpTimeCounter > 0)
+        {
+            _rigidbody.velocity = Vector2.up * _jumpPower;
+            _jumpTimeCounter -= Time.deltaTime;
+        }
+        else
+        {
+            _isJumping = false;
+        }
     }
 
     //private void FacingDirection()
@@ -63,15 +116,28 @@ public class PlayerController : MonoBehaviour
     //}
     private void FixedUpdate()
     {
-        _rigidbody.velocity = _direction *_speed* Time.deltaTime;
-
+        _rigidbody.velocity = new Vector2(_direction.x *_speed* Time.deltaTime,_rigidbody.velocity.y);
         //For later add clear method call for groundcheck, check neon runner perhaps and do callculations through parameters
-        if (_isGrounded && _isTryingToJump)
-        {
-            _rigidbody.AddForce(new Vector2(0f,_jumpPower));
-            Debug.Log("Jumpy Jump");
-            //_rigidbody.velocity += Vector2.up * _jumpPower;
-        }
+    }
+
+    private void Jump()
+    {
+        //Vector2 newVelocity = new Vector2();
+        //newVelocity.x = _rigidbody.velocity.x;
+        //newVelocity.y = _jumpPower;
+        //_rigidbody.AddForce(new Vector2(0f,_jumpPower));
+
+        //_rigidbody.velocity = newVelocity;
+        _isJumping = true;
+        _jumpTimeCounter = _jumpTime;
+        _rigidbody.velocity = Vector2.up * _jumpPower;
+        _rigidbody.gravityScale = _fallingGravity;
+        Debug.Log("Jumpy Jump");
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(_groundCheckPos.position, _groundCheckRadius);
     }
 
     private void UpdatePlayerInput()
@@ -109,7 +175,7 @@ public class PlayerController : MonoBehaviour
         {
             moveX -= 1f;
         }
-        //Debug.Log(moveX);
+
         _direction = new Vector2(moveX, moveY).normalized;
     }
 }
